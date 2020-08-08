ad3b3ba7438f4c8d6182c5d385e0c36ef84d6d8e
jczhangwei <jczhangwei@foxmail.com>
2020-08-09 01:52:22 +0800

    add

--- unity/Assets/JsBehaviour.cs
+++ unity/Assets/JsBehaviour.cs
@@ -2,19 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
 using System.Linq.Expressions;
 using V8.Net;
+using System.Text;
+using System.Reflection;
 
 public class JsBehaviour : MonoBehaviour
 {
     public TextAsset jsScript;
 
-    internal static V8Engine engine = new V8Engine(); //all js behaviour shared one jsenv only!
+    internal static V8Engine engine = new V8Engine();
 
     private Handle jsBehaviour;
 
+    internal static bool isInited = false;
+
+    string ReadFile(string path)
+    {
+        var strs = File.ReadAllLines(path);
+        StringBuilder sb = new StringBuilder();
+        foreach (var s in strs)
+        {
+            sb.Append(s);
+            sb.Append("\n");
+        }
+
+        return sb.ToString();
+    }
+
+    void initEnvroment()
+    {
+        if (!isInited)
+        {
+            var b = Assembly.Load("UnityEngine").GetTypes();
+            Debug.Log(b.Length);
+            foreach (var t in b)
+            {
+                Debug.Log(t);
+            }
+
+            var a = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var t in a)
+            {
+                Debug.Log(t);
+            }
+        }
+    }
+
     void Awake()
     {
+        initEnvroment();
+
         engine.RegisterType<Debug>(null, true, ScriptMemberSecurity.Locked); // (this line is NOT required, but allows more control over the settings)
         engine.GlobalObject.SetProperty(typeof(Debug));
 
@@ -26,9 +65,11 @@ public class JsBehaviour : MonoBehaviour
             @"
                 Debug.Log('dddd');
                 Debug.Log(typeof Vector2);
-                for(var i = 0;i<100;i++){
-                    Vector2.zero.ToString();
+                var d = (new Date()).getTime();
+                for(var i = 0;i<1000;i++){
+                    var b = {};
                 }
+                Debug.Log('using time: ' + ((new Date()).getTime()-  d));
                 Debug.Log(Vector2.zero);
             "
             );
